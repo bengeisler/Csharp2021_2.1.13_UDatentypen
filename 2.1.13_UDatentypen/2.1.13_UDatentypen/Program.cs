c625b6514e276b9a0da8ec4a81f3b5b3fe6aaa37
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-01-31 16:24:47 +0100

    Projekt erstellt

---
+++ 2.1.13_UDatentypen/2.1.13_UDatentypen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._1._13_UDatentypen
+{
+	static class Program
+	{
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
